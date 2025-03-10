ca090d6c02f724feb2098eccfc912d1db03a38a0
Ruramai <ruramaimuchenga@gmail.com>
2025-03-10 16:54:24 +0400

    Added StockVision overview page

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//     console.log("StockVision AI Loaded!");
+// });
+
+
+
+
 document.addEventListener("DOMContentLoaded", function() {
     console.log("StockVision AI Loaded!");
+
+    // Function to navigate to different pages
+    window.navigateTo = function(page) {
+        window.location.href = page;
+    };
 });
